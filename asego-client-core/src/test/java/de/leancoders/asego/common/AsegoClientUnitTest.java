4c84c29138916c69e749335a74cb7902e784fc10
Marcus Schüttler <schuettler.marcus@gmail.com>
2025-04-11 11:09:48 +0200

    refector use constants in test

--- asego-client-core/src/test/java/de/leancoders/asego/common/AsegoClientUnitTest.java
+++ asego-client-core/src/test/java/de/leancoders/asego/common/AsegoClientUnitTest.java
@@ -1,6 +1,5 @@
 package de.leancoders.asego.common;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.leancoders.asego.client.helper.jackson.ObjectMapperFactory;
 import de.leancoders.asego.client.model.internal.AsegoConfig;
@@ -9,13 +8,15 @@ import de.leancoders.asego.common.response.customer.CustomerSearchResponse;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 
-
 /**
  *
  */
 @Log4j2
 public class AsegoClientUnitTest {
 
+    private static final String USERNAME = "test";
+    private static final String PASSWORD = "XxyL8X1GT6";
+    private static final String CUSTOMER_EMAIL = "test@test.de";
     
     public static final AsegoConfig ASEGO_CONFIG_DEFAULT =
         AsegoConfig.of(
@@ -30,7 +31,7 @@ public class AsegoClientUnitTest {
             new AsegoClientService(
                 ASEGO_CONFIG_DEFAULT
             );
-        clientService.login("test", "XxyL8X1GT6");
+        clientService.login(USERNAME, PASSWORD);
     }
 
     @Test
@@ -38,13 +39,12 @@ public class AsegoClientUnitTest {
         final AsegoClientService clientService = new AsegoClientService(
             ASEGO_CONFIG_DEFAULT
         );
-        clientService.login("test", "XxyL8X1GT6");
+        clientService.login(USERNAME, PASSWORD);
 
         final CustomerSearchResponse customers =
             clientService
                 .customers()
-                .searchByEmail(null, 0, 10, "test@test.de");
-
+                .searchByEmail(null, 0, 10, CUSTOMER_EMAIL);
 
         System.out.println("customers = " + customers);
     }
